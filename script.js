0daf4457cc4f6b797827a24b181eb2fe16f0a05b
test <test>
2022-11-19 16:37:39 +0500

    js

---
+++ script.js
@@ -0,0 +1,16 @@
+ var pul = +prompt("qancha pulga ovqatlanmoqchisiz?")
+ if (pul > 35000) {
+     alert("gref qi pulin kopakanu")
+ } else if (pul >= 35000 && pul < 30000) {
+     alert("siz lavash va cocacola ichishingiz mumkin")
+ } else if (pul < 25000 && pul >= 20000) {
+     alert("siz lavash yiyishingiz mumkin")
+ } else if (pul < 20000 && pul >= 17000) {
+
+     alert("siz hot-dog va coca cola ichishingiz mumkin")
+ } else if (pul < 4500) {
+     alert("gaday rolton ol")
+ }
+ else if (pul < 0) {
+    alert("uyga go ")
+}
